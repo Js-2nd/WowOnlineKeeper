bbd365ccc6801cd1a9e9410dd4966ad184024a25
Js <JimmyLoveSiren@users.noreply.github.com>
2019-08-31 00:24:24 +0900

    v0.1.1

--- WowOnlineKeeper/Program.cs
+++ WowOnlineKeeper/Program.cs
@@ -9,7 +9,7 @@
 
 	class Program
 	{
-		public const string Version = "0.1.0";
+		public const string Version = "0.1.1";
 
 		static async Task Main()
 		{
@@ -21,7 +21,10 @@
 		readonly Random m_Random = new Random();
 		Dictionary<int, Item> m_Items = new Dictionary<int, Item>();
 		Dictionary<int, Item> m_ItemsBuf = new Dictionary<int, Item>();
+		DateTime m_LastInputTime = DateTime.Now;
 		DateTime m_Now;
+
+		bool AwayFromKeyboard => m_Now - m_LastInputTime >= TimeSpan.FromMinutes(5);
 
 		async Task Run()
 		{
@@ -32,17 +35,19 @@
 			};
 			while (true)
 			{
-				m_Now = DateTime.UtcNow;
+				m_Now = DateTime.Now;
 				UpdateItems();
 				foreach (var item in m_Items.Values)
 				{
-					if (m_Now - item.LastInputTime >= TimeSpan.FromSeconds(5))
+					if (m_Now - item.LastInputTime >= TimeSpan.FromSeconds(30))
 					{
-						Console.WriteLine(DateTime.Now);
+						Console.WriteLine(m_Now);
 						item.LastInputTime = m_Now;
 						GetWindowRect(item.Process.MainWindowHandle, out var rect);
-						await item.Click(((rect.right - rect.left) / 2, (int) ((rect.bottom - rect.top) * 0.917)));
-						await item.Key(RandomKey());
+						Point point = ((rect.right - rect.left) / 2, (int) ((rect.bottom - rect.top) * 0.917));
+						bool awayFromKeyboard = AwayFromKeyboard;
+						await item.Click(point, awayFromKeyboard ? 100 : 10);
+						await item.Key(awayFromKeyboard ? RandomKey() : VirtualKey.VK_SPACE);
 					}
 				}
 
@@ -52,6 +57,7 @@
 
 		void OnUserInput()
 		{
+			m_LastInputTime = m_Now;
 			GetWindowThreadProcessId(GetForegroundWindow(), out int processId);
 			if (m_Items.TryGetValue(processId, out var item)) item.LastInputTime = m_Now;
 		}
@@ -89,28 +95,25 @@
 		public Process Process;
 		public DateTime LastInputTime;
 
-		public async Task Key(VirtualKey key)
+		public async Task Key(VirtualKey key, int delay = 50)
 		{
 			if (key == VirtualKey.VK_NO_KEY) return;
 			PostMessage(Process.MainWindowHandle, WindowMessage.WM_IME_KEYDOWN, (IntPtr) key, IntPtr.Zero);
-			await Task.Delay(50);
+			await Task.Delay(delay);
 			PostMessage(Process.MainWindowHandle, WindowMessage.WM_IME_KEYUP, (IntPtr) key, IntPtr.Zero);
 		}
 
-		public async Task Click(Point point)
+		public async Task Click(Point point, int delay = 50)
 		{
 			GetCursorPos(out var pos);
-			await Task.Delay(25);
 			POINT pt = point;
 			ClientToScreen(Process.MainWindowHandle, ref pt);
 			SetCursorPos(pt.x, pt.y);
-			await Task.Delay(25);
 			var lParam = point.ToLParam();
 			PostMessage(Process.MainWindowHandle, WindowMessage.WM_MOUSEMOVE, IntPtr.Zero, lParam);
-			await Task.Delay(25);
 			PostMessage(Process.MainWindowHandle, WindowMessage.WM_LBUTTONDOWN, (IntPtr) 1, lParam);
 			PostMessage(Process.MainWindowHandle, WindowMessage.WM_LBUTTONUP, IntPtr.Zero, lParam);
-			await Task.Delay(25);
+			await Task.Delay(delay);
 			SetCursorPos(pos.x, pos.y);
 		}
 	}
